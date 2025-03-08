28c30efa6a3abc7172c9fb961ffbda2ef10f132f
champiww <a24sergiobc@iessanclemente.net>
2025-03-08 18:15:53 +0100

    product list

--- listApp/models.py
+++ listApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    price = models.IntegerField()
+    picture = models.ImageField(upload_to='listApp/images/')
+    slug = models.SlugField(default='', null=False, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- listApp/views.py
+++ listApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, Product
 
 # Create your views here.
+def home(request):
+    return render(request, 'list/home.html')
+
+class productListView(ListView):
+    template_name = "list/list.html"
+    model = Product
+    context_object_name = "products"
